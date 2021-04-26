00c941e13d63e0e50549a79164fda8b665df03e3
Alessandro CITARELLA <prof.alessandro.citarella@isspitagora.edu.it>
2021-04-26 15:46:52 +0200

    Versione iniziale dell'applicazione ver.01.00

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module it.edu.isspitagora.provagit {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens it.edu.isspitagora.provagit to javafx.fxml;
+    exports it.edu.isspitagora.provagit;
+}
+*/
