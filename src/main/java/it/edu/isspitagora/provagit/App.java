913ce5c3bc2c99be742759b2a76da66423cb65ff
Alessandro CITARELLA <prof.alessandro.citarella@isspitagora.edu.it>
2021-04-26 15:52:25 +0200

    Versione dell'applicazione ver.01.01 Inserito nuovo membro ...

--- src/main/java/it/edu/isspitagora/provagit/App.java
+++ src/main/java/it/edu/isspitagora/provagit/App.java
@@ -14,6 +14,8 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
+    private String nome;
+    
 
     @Override
     public void start(Stage stage) throws IOException {
